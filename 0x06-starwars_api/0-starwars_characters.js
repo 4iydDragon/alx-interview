435f1d8edd680421c72882492a6db06c4a69f1bf
Emmanuel <emmanuel.k.kangogo@gmail.com>
2024-04-27 22:41:59 +0300

    0x06 & 0x05

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,41 @@
 #!/usr/bin/node
-
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+if (process.argv.length <= 2) {
+  console.log('Usage: node script.js <film-id>');
+  process.exit(1);
+}
+
+const filmId = process.argv[2];
+const filmUrl = `${API_URL}/films/${filmId}/`;
+
+request.get(filmUrl, (err, response, body) => {
+  if (err || response.statusCode !== 200) {
+    console.error('Error:', err || body);
+    process.exit(1);
+  }
+
+  const characters = JSON.parse(body).characters;
+  const promises = characters.map((url) => {
+    return new Promise((resolve, reject) => {
+      request.get(url, (err, response, body) => {
+        if (err || response.statusCode !== 200) {
+          reject(err || body);
+        } else {
+          const characterName = JSON.parse(body).name;
+          resolve(characterName);
+        }
+      });
+    });
   });
-};
+
+  Promise.all(promises)
+    .then((names) => {
+      console.log(names.join('\n'));
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      process.exit(1);
+    });
+});
